d2bee59340e48b1d1fcbe1d7858e3950a51d102b
fifty <mohamedelameen50>
2021-11-03 16:34:38 +0400

    manage icons

--- lib/utils/binding.dart
+++ lib/utils/binding.dart
@@ -1,4 +1,5 @@
 import 'package:get/get.dart';
+import 'package:weather_app/viewModel/clock_view_model.dart';
 import 'package:weather_app/viewModel/weather_view_model.dart';
 
 class Binding extends Bindings{
@@ -6,6 +7,7 @@ class Binding extends Bindings{
   @override
   void dependencies() {
     Get.lazyPut(() => WeatherViewModel());
+    Get.lazyPut(() => ClockViewModel());
   }
 
 
--- lib/view/widgets/current_time_icon.dart
+++ lib/view/widgets/current_time_icon.dart
@@ -1,11 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:lottie/lottie.dart';
+import 'package:weather_app/utils/icons_extension.dart';
 
-class WeatherIcon extends StatelessWidget {
-  const WeatherIcon({Key? key}) : super(key: key);
+class WeatherAnimatedIcon extends StatelessWidget {
+  final String iconCode;
+
+  const WeatherAnimatedIcon({Key? key, required this.iconCode})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Lottie.asset('assets/animations/night.json', fit: BoxFit.contain, width: 128);
+    return Lottie.asset('assets/animations/$iconCode.json',
+        fit: BoxFit.contain, width: 128);
+  }
+}
+
+class WeatherIcon extends StatelessWidget {
+  final String iconCode;
+
+  const WeatherIcon({Key? key, required this.iconCode}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SvgPicture.asset(
+      'assets/icons/${IconAsset.fromIconCode(iconCode)}',
+      fit: BoxFit.contain,
+      color: Colors.white,
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.height /5,
+      alignment: Alignment.center,
+    );
   }
 }
